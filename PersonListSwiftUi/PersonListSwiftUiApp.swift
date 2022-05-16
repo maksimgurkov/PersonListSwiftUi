97843f6cc33188f9a21153fd03f2c47e63162711
Maksim Gurkov <maksim.gurkov@gmail.com>
2022-05-16 13:00:39 +0300

    Initial Commit

---
+++ PersonListSwiftUi/PersonListSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonListSwiftUiApp.swift
+//  PersonListSwiftUi
+//
+//  Created by Максим Гурков on 16.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct PersonListSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
